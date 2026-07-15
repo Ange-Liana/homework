c261fe51239b1199c0525fc9fed3364be2a4e006
Liana-Batsinde <l.batsinde@alustudent.com>
2026-07-16 01:55:45 +0200

    Script for adding iptables firewall rules
    Removed comments for clarity and streamlined the firewall rules script.

--- firewall_rules.sh
+++ firewall_rules.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Clear existing rules
+
 iptables -F
 
-# Default policies
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
@@ -13,31 +12,25 @@ HOST=$(hostname)
 if [ "$HOST" = "web-01" ] || [ "$HOST" = "web-02" ]; then
     echo "Configuring firewall for $HOST..."
 
-    # Allow SSH from anywhere
     iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
-    # Allow HTTP only from the Load Balancer
     iptables -A INPUT -p tcp -s 172.25.0.10 --dport 80 -j ACCEPT
 
-    # Allow loopback interface
     iptables -A INPUT -i lo -j ACCEPT
 
-    # Allow established connections
     iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 elif [ "$HOST" = "lb-01" ]; then
     echo "Configuring firewall for $HOST..."
 
-    # Allow SSH from anywhere
     iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
-    # Allow HTTPS from anywhere
+
     iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
-    # Allow loopback interface
+    
     iptables -A INPUT -i lo -j ACCEPT
 
-    # Allow established connections
     iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 else
